823d395772c18cf5a52c20be6b1bce3fe55a3ed8
TakumiHiguchi <andf.for.gt@gmail.com>
2020-10-10 11:34:32 +0900

    ref: apiを叩く部分をbase_wrokerに切り出した

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,24 +1,12 @@
 class ArticleController < ApplicationController
-    def show
-        require 'openssl'
-        require 'net/https'
-        @ua = request.env["HTTP_USER_AGENT"]
-        #prooflyからuser_key=music.branchwithの記事一覧を取得する
-        uri = URI.parse("https://mbwapi.herokuapp.com/")
-        https = Net::HTTP.new(uri.host, uri.port)
-        https.open_timeout = 10
-        https.read_timeout = 10
-        https.use_ssl = true
-        https.verify_mode = OpenSSL::SSL::VERIFY_PEER
-        https.verify_depth = 5
-    
-        begin
-          response = nil
-          https.start do
-            response = https.get("/api/v1/mbw/article/" + params[:id])
-          end
-          @article=JSON.parse(response.body)
-          
-        end
+  def show
+    base = BaseWorker.new
+    response = base.hit_mbw_api({ url: "/api/v1/mbw/article/" + params[:id] })
+    if response.present?
+      @ua = request.env["HTTP_USER_AGENT"]
+      @article = JSON.parse(response.body)
+    else
+      render status: 404
     end
+  end
 end
---
+++ app/workers/baseWorker.rb
@@ -0,0 +1,27 @@
+class BaseWorker
+  def get_key
+    o = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map { |i| i.to_a }.flatten
+    return (0...20).map { o[rand(o.length)] }.join
+  end
+  
+  def hit_mbw_api(props)
+    require 'openssl'
+    require 'net/https'
+
+    uri = URI.parse("https://mbwapi.herokuapp.com/")
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.open_timeout = 10
+    https.read_timeout = 10
+    https.use_ssl = true
+    https.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    https.verify_depth = 5
+
+    begin
+      response = nil
+      https.start do
+        response = https.get(props[:url])
+      end
+      return response
+    end
+  end
+end
