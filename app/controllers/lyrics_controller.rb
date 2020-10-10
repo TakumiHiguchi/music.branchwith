07ee985017c58e0a37a901cb37290a31d795644a
TakumiHiguchi <andf.for.gt@gmail.com>
2020-10-10 11:40:35 +0900

    ref: lyric_controllerのapiを叩く部分をbase_wrokerに切り出した

--- app/controllers/lyrics_controller.rb
+++ app/controllers/lyrics_controller.rb
@@ -1,44 +1,14 @@
 class LyricsController < ApplicationController
-  def index
-    require 'openssl'
-    require 'net/https'
-
-    #prooflyからuser_key=music.branchwithの記事一覧を取得する
-    uri = URI.parse("https://mbwapi.herokuapp.com/")
-    https = Net::HTTP.new(uri.host, uri.port)
-    https.open_timeout = 10
-    https.read_timeout = 10
-    https.use_ssl = true
-    https.verify_mode = OpenSSL::SSL::VERIFY_PEER
-    https.verify_depth = 5
-    response = nil
-    https.start do
-      response = https.get("/api/v1/webgui/search?model=lyric&limit=20")
-    end
-    @lyricsData=JSON.parse(response.body)
-  end
-
   def show
-    require 'openssl'
-    require 'net/https'
-    @ua = request.env["HTTP_USER_AGENT"]
-    #prooflyからuser_key=music.branchwithの記事一覧を取得する
-    uri = URI.parse("https://mbwapi.herokuapp.com/")
-    https = Net::HTTP.new(uri.host, uri.port)
-    https.open_timeout = 10
-    https.read_timeout = 10
-    https.use_ssl = true
-    https.verify_mode = OpenSSL::SSL::VERIFY_PEER
-    https.verify_depth = 5
-    response = nil
-    response1 = nil
-    https.start do
-      response = https.get("/api/v1/webgui/lyrics/"+params[:id])
+    base = BaseWorker.new
+    response_lyrics = base.hit_mbw_api({ url: "/api/v1/webgui/lyrics/"+params[:id] })
+    response_lyrics_list = base.hit_mbw_api({ url: "/api/v1/webgui/search?model=lyric&limit=20" })
+    if response.present?
+      @ua = request.env["HTTP_USER_AGENT"]
+      @lyric = JSON.parse(response_lyrics.body)
+      @poplyric = JSON.parse(response_lyrics_list.body)
+    else
+      render status: 404
     end
-    https.start do
-      response1 = https.get("/api/v1/webgui/search?model=lyric&limit=20")
-    end
-    @poplyric=JSON.parse(response1.body)
-    @lyric=JSON.parse(response.body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get '/' => 'home#index'
   resources :article, only: [:show]
   resources :tag, only: [:index,:show]
-  resources :lyrics, only: [:index,:show]
+  resources :lyrics, only: [:show]
   resources :search, only: [:index]
 end
