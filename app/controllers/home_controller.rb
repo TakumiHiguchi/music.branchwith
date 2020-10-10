a8df57c2a5dd4b019b72c3d957a7faaad7e9b51a
TakumiHiguchi <andf.for.gt@gmail.com>
2020-10-11 06:30:06 +0900

    ref: baseworkerのapiを叩く部分を環境ごとにurlを変えるように変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
     base = BaseWorker.new
-    response_article = base.hit_mbw_api({ url: "/api/v1/mbw/article?limit=20" })
-    response_lyrics = base.hit_mbw_api({ url: "/api/v1/webgui/search?model=lyric&limit=20" })
+    response_article = base.hit_mbw_api({ url: "/api/v1/mbw/article", params: { limit: 20 } })
+    response_lyrics = base.hit_mbw_api({ url: "/api/v1/webgui/search", params: { limit: 20, model: 'lyric' } })
     if response.present?
       @articleDatas = JSON.parse(response_article.body)
       @poplyric = JSON.parse(response_lyrics.body)
--- app/workers/baseWorker.rb
+++ app/workers/baseWorker.rb
@@ -5,22 +5,48 @@ class BaseWorker
   end
   
   def hit_mbw_api(props)
-    require 'openssl'
-    require 'net/https'
+    case Rails.env
+      when 'test' then
+        uri = URI.parse("http://localhost:3020" + props[:url])
+        uri.query = URI.encode_www_form(props[:params])
+        http = http_setting(uri)
+        begin
+          http.start do
+            return http.get(uri.path + '?' + uri.query)
+          end
+        end
+      when 'development' then
+        uri = URI.parse("http://localhost:3020" + props[:url])
+        uri.query = URI.encode_www_form(props[:params])
+        http = http_setting(uri)
+        begin
+          http.start do
+            return http.get(uri.path + '?' + uri.query)
+          end
+        end
+      when 'production' then
+        uri = URI.parse("https://mbwapi.herokuapp.com" + props[:url])
+        uri.query = URI.encode_www_form(props[:params])
+        https = https_setting(uri)
+        https.start do
+          return https.get(uri.path + '?' + uri.query)
+        end
+    end
+  end
 
-    uri = URI.parse("https://mbwapi.herokuapp.com/")
+  def https_setting(uri)
     https = Net::HTTP.new(uri.host, uri.port)
     https.open_timeout = 10
     https.read_timeout = 10
     https.use_ssl = true
     https.verify_mode = OpenSSL::SSL::VERIFY_PEER
     https.verify_depth = 5
-    begin
-      response = nil
-      https.start do
-        response = https.get(props[:url])
-      end
-      return response
-    end
+    return https
+  end
+  def http_setting(uri)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.open_timeout = 10
+    http.read_timeout = 10
+    return http
   end
 end
