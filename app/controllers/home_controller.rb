5cc00b683c7e9c286116cb8036e65a594585d886
TakumiHiguchi <andf.for.gt@gmail.com>
2020-10-10 11:37:48 +0900

    ref: home_controllerのapiを叩く部分をbase_wrokerに切り出した

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,29 +1,13 @@
 class HomeController < ApplicationController
   def index
-    require 'openssl'
-    require 'net/https'
-    require 'net/http'
-    #prooflyからuser_key=music.branchwithの記事一覧を取得する
-    uri = URI.parse("https://mbwapi.herokuapp.com/")
-    https = Net::HTTP.new(uri.host, uri.port)
-    https.open_timeout = 10
-    https.read_timeout = 10
-    https.use_ssl = true
-    https.verify_mode = OpenSSL::SSL::VERIFY_PEER
-    https.verify_depth = 5
-
-    begin
-      response = nil
-      response1 = nil
-      https.start do
-        response = https.get("/api/v1/mbw/article?limit=20")
-      end
-      @articleDatas=JSON.parse(response.body)
-      https.start do
-        response1 = https.get("/api/v1/webgui/search?model=lyric&limit=20")
-      end
-      @poplyric=JSON.parse(response1.body)
-      
+    base = BaseWorker.new
+    response_article = base.hit_mbw_api({ url: "/api/v1/mbw/article?limit=20" })
+    response_lyrics = base.hit_mbw_api({ url: "/api/v1/webgui/search?model=lyric&limit=20" })
+    if response.present?
+      @articleDatas = JSON.parse(response_article.body)
+      @poplyric = JSON.parse(response_lyrics.body)
+    else
+      render status: 404
     end
   end
 end
