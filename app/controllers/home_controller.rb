6c36dc97659c3c594f85faa86051335ec2d8413a
TakumiHiguchi <andf.for.gt@gmail.com>
2020-10-28 15:45:38 +0900

    ref: サムネイルがない時にnoimageの画像を表示するように変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     base = BaseWorker.new
     response_article = base.hit_mbw_api({ url: "/api/v1/mbw/article", params: { limit: 20 } })
-    response_lyrics = base.hit_mbw_api({ url: "/api/v1/webgui/search", params: { limit: 20, model: 'lyric' } })
+    response_lyrics = base.hit_mbw_api({ url: "/api/v1/mbw/search", params: { limit: 20, model: 'lyric' } })
     if response.present?
       @articleDatas = JSON.parse(response_article.body)
       @poplyric = JSON.parse(response_lyrics.body)
