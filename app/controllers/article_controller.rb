a85ae5bce2eb951ab860adb3ceb12661352ad455
TakumiHiguchi <andf.for.gt@gmail.com>
2020-11-04 13:05:23 +0900

    add: 404等の分岐を作成

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -4,7 +4,16 @@ class ArticleController < ApplicationController
     response = base.hit_mbw_api({ url: "/api/v1/mbw/article/" + params[:id] })
     if response.present?
       @ua = request.env["HTTP_USER_AGENT"]
-      @article = JSON.parse(response.body)
+      case response.code.to_i
+      when 200
+        @article = JSON.parse(response.body)
+      when 301
+        redirect_to '/article/' + JSON.parse(response.body)["key"], status: 301
+      when 404
+        render status: 404
+      else
+        render status: 500
+      end
     else
       render status: 404
     end
