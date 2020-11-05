5d29140325696fc5dc7f19f0f55c4825db1c59bc
TakumiHiguchi <andf.for.gt@gmail.com>
2020-11-06 07:36:49 +0900

    add: google adとgoogle アナリティクスのタグを設置

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
     response_article = base.hit_mbw_api({ url: "/api/v1/mbw/article", params: { limit: 20 } })
     response_lyrics = base.hit_mbw_api({ url: "/api/v1/mbw/search", params: { limit: 20, model: 'lyric' } })
     if response.present?
+      @ua = request.env["HTTP_USER_AGENT"]
       @articleDatas = JSON.parse(response_article.body)
       @poplyric = JSON.parse(response_lyrics.body)
     else
