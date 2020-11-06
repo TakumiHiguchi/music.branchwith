106eb47e5db3d67caa529f534473092c70d43d1b
TakumiHiguchi <andf.for.gt@gmail.com>
2020-11-06 18:55:02 +0900

    ref: googleアドセンスの設定

---
+++ app/javascript/google/adsence.js
@@ -0,0 +1,13 @@
+document.addEventListener('turbolinks:load', function () {
+  var ads = document.querySelectorAll('.adsbygoogle');
+
+  if (ads.length > 0) {
+    ads.forEach(function (ad) {
+      if (ad.firstChild) {
+        ad.removeChild(ad.firstChild);
+      }
+      window.adsbygoogle = window.adsbygoogle || [];
+      window.adsbygoogle.push({});
+    });
+  }
+})
