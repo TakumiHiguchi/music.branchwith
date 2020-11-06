9c0dae3b7e3fade0e7a5e003019200c447ddb2e5
TakumiHiguchi <andf.for.gt@gmail.com>
2020-11-07 04:12:50 +0900

    fix: net/httpを追加

--- app/workers/baseWorker.rb
+++ app/workers/baseWorker.rb
@@ -1,4 +1,7 @@
 class BaseWorker
+  require 'net/http'
+  require 'net/https'
+
   def get_key
     o = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map { |i| i.to_a }.flatten
     return (0...20).map { o[rand(o.length)] }.join
